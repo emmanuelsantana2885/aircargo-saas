29e66645acedcb72e4d421311899a7471db6da54
emmanuelsantana2885 <emmanuelsantana2885@gmail.com>
2026-06-08 18:17:46 -0400

    feat:Spring Boot project scaffold - JPA, Flyway, Actuator

---
+++ backend/aircargo-api/src/main/java/com/aircargo/AircargoApiApplication.java
@@ -0,0 +1,12 @@
+package com.aircargo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AircargoApiApplication {
+
+	public static void main(String[] args){
+	 	SpringApplication.run(AircargoApiApplication.class, args);
+	}
+}
---
+++ backend/aircargo-api/src/test/java/com/aircargo/AppTest.java
@@ -0,0 +1,13 @@
+package com.aircargo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTest {
+
+    @Test
+    void contextLoads() {
+        // Verifica que el contexto de Spring arranca correctamente
+    }
+}
